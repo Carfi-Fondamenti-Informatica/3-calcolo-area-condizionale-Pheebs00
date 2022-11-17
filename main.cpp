d1f9013fe10bdc3ce6366b064b3e9089c5276261
Pheebs00 <115625720+Pheebs00@users.noreply.github.com>
2022-11-17 17:49:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+    float a=0, b=0, risultato=0;
+    int opzione =0;
+    cin>>a>>b>>opzione;
+    switch (opzione){
+        case 0:
+            risultato=a*b/2;
+            cout<<risultato;
+            break;
+        case 1:
+            risultato=a*a;
+            cout <<risultato;
+            break;
+        case 2:
+            risultato=a*b;
+            cout<<risultato;
+            break;
+        default:
+            cout <<"opzione non valida"<<endl;
+    }
+    return 0;
 }
